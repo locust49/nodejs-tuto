404872f7539c004b88126e309a0be8b2804dbe88
locust49 <lyazid.souad@gmail.com>
2021-03-09 23:04:15 +0100

    basic routing

--- VID#04/server.js
+++ VID#04/server.js
@@ -8,8 +8,25 @@ const server = http.createServer((request, response) => {
 	// set header content type
 	response.setHeader('Content-Type', 'text/html');
 
+	let path = './views/';
+	switch(request.url)
+	{
+		case '/':
+			path += 'index.html';
+			break;
+		case '/index':
+			path += 'index.html';
+			break;
+		case '/about':
+			path += 'about.html';
+			break;
+		default :
+			path += '404.html';
+			break;
+	}
+
 	//send an html file
-	fs.readFile('./views/index.html', (err, data) => {
+	fs.readFile(path, (err, data) => {
 		if (err)
 			console.log(err);
 		else
