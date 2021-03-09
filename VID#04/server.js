22cea377b035545bbccebde334b2047dd139c982
locust49 <lyazid.souad@gmail.com>
2021-03-09 22:41:20 +0100

    returning html page

--- VID#04/server.js
+++ VID#04/server.js
@@ -1,17 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
 	// request is a huuuge object, let's log some of its attributes..
 	console.log(request.url, request.method);
 
 	// set header content type
-		// response.setHeader('Content-Type', 'text/plain');
-		// response.write('Salam =) ');
 	response.setHeader('Content-Type', 'text/html');
-	response.write('<head><link rel="stylesheet" href="#"')
-	response.write('<p>Salaaam al3alam</p>')
-	response.write('<p>Salaaam again al3alam</p>')
-	response.end();
+
+	//send an html file
+	fs.readFile('./views/index.html', (err, data) => {
+		if (err)
+			console.log(err);
+		else
+			response.write(data);
+		response.end(); // or response.end(data) if we only got 'data'
+	});
 });
 
 server.listen(3000, 'localhost', () =>{
