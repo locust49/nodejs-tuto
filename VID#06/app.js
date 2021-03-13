fc5611e76732bbd58540e22f019e9a2e26df20f4
locust49 <lyazid.souad@gmail.com>
2021-03-13 20:58:20 +0100

    redirections

--- VID#06/app.js
+++ VID#06/app.js
@@ -7,15 +7,26 @@ const app = express();
 app.listen(3000);
 
 // get responses
-app.get('/', (request, response) => {
 
+app.get('/', (request, response) => {
+	
 	// doesnt need to specify type content (setHeader)
 	// and set the status code auto
 		//response.send('<p>home page</p>'); 
-	// we should specify the path of the root where the files are located.
+		// we should specify the path of the root where the files are located.
 	response.sendFile('./views/index.html', { root: __dirname + '/../VID#04/' });
 });
 
 app.get('/about', (request, response) => {
 	response.sendFile('./views/about.html', { root: __dirname + '/../VID#04/' });
+});
+
+app.get('/about-us', (request, response) => {
+	response.redirect('/about');
+});
+
+// fire for every url other than the one above
+
+app.use((request, response) => {
+	response.status(404).sendFile('./views/404.html', { root: __dirname + '/../VID#04/'});
 });
