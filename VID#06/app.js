af857316bf92a02719b2fc897643d474d774fcea
locust49 <lyazid.souad@gmail.com>
2021-03-13 19:27:58 +0100

    routing and html page

--- VID#06/app.js
+++ VID#06/app.js
@@ -11,7 +11,11 @@ app.get('/', (request, response) => {
 
 	// doesnt need to specify type content (setHeader)
 	// and set the status code auto
-	response.send('<p>home page</p>'); 
+		//response.send('<p>home page</p>'); 
+	// we should specify the path of the root where the files are located.
+	response.sendFile('./views/index.html', { root: __dirname + '/../VID#04/' });
+});
 
-
+app.get('/about', (request, response) => {
+	response.sendFile('./views/about.html', { root: __dirname + '/../VID#04/' });
 });
