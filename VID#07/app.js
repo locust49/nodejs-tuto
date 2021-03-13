933cf7be1f5537b9a649b348427b9c3bedcb6a6d
locust49 <lyazid.souad@gmail.com>
2021-03-13 23:49:39 +0100

    passing data into views

--- VID#07/app.js
+++ VID#07/app.js
@@ -9,17 +9,23 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (request, response) => {
-	response.render('index');
+	// const blogs = [
+	// 	{title: 'Title1', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+	// 	{title: 'Title2', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+	// 	{title: 'Title3', snippet: 'Lorem ipsum dolor sit amet consectetur'}
+	// ];
+	const blogs = [];
+	response.render('index', { title: 'Home', blogs});
 });
 
 app.get('/about', (request, response) => {
-	response.render('about');
+	response.render('about', { title: 'About' });
 });
 
 app.get('/blogs/create', (request, response) => {
-	response.render('create');
+	response.render('create', { title: 'Create' });
 })
 
 app.use((request, response) => {
-	response.status(404).render('404');
+	response.status(404).render('404', { title: '404' });
 });
